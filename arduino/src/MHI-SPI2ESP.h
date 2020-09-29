c57b740e4e52d88f03708d42103c96b3fce6b7cf
Christoph Mueller <christoph@flying-lama.com>
2020-09-29 08:04:02 +0200

    init

---
+++ arduino/src/MHI-SPI2ESP.h
@@ -0,0 +1,5 @@
+void setup();
+bool verify_checksum();
+void update_checksum();
+void softwareReset();
+void SPI_sync();
---
+++ wifi/src/MHI-ESP2MQTT.h
@@ -0,0 +1,5 @@
+void callback(char*, uint8_t*, unsigned int);
+void saveConfigCallback();
+void setup();
+void connect();
+void loop();
